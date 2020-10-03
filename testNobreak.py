825b98eb29e96c2c284f45bfb71adbc74fdd7864
Udderbutter <72292298+Udderbutter@users.noreply.github.com>
2020-10-03 04:36:08 -0700

    Update testNobreak.py

--- testNobreak.py
+++ testNobreak.py
@@ -2,9 +2,13 @@ import random
 
 Bowser_is_best = ['Did your mother Chris Brown you as a baby? You fucking retard?', 'Your opinion is shit', 'Ding dong your opinion is wrong', 'Fucking kill yourself', 'Listen here you clueless shit where the fuckdoyouliveiwillcometoyourfuckinghouserightnowa-',\
                   'Go fuck yourself', 'You were adopted', 'Nobody loves you', 'You will die alone', 'Stop breathing']
-
+Time = 2
 while True:
     x = input ('Who in the SSBM cast is most DOPE?').lower()
+    if Time == 0:
+      print ('You are helpless. Goodbye')
+      break
+    Time -=1
     if x == "":
         break
     if x == 'bowser':
