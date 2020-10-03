99290cc5de032693e56f795caf85a01d1dd57547
Udderbutter <72292298+Udderbutter@users.noreply.github.com>
2020-10-02 22:30:11 -0700

    Add files via upload

---
+++ testNobreak.py
@@ -0,0 +1,20 @@
+import random
+
+Bowser_is_best = ['Did your mother Chris Brown you as a baby? You fucking retard?', 'Your opinion is shit', 'Ding dong your opinion is wrong', 'Fucking kill yourself', 'Listen here you clueless shit where the fuckdoyouliveiwillcometoyourfuckinghouserightnowa-',\
+                  'Go fuck yourself', 'You were adopted', 'Nobody loves you', 'You will die alone', 'Stop breathing']
+
+while True:
+    x = input ('Who in the SSBM cast is most DOPE?').lower()
+    if x == "":
+        break
+    if x == 'bowser':
+        print ('MY MAN!')
+        break
+    elif x == 'falco':
+        print ('You aight')
+        break
+    elif x == 'ice climbers':
+        print ('This isnt even funny at this point like seriously just go dive off of a cliff you filthy fucking degenerate')
+        break
+    else:
+        print (random.choice(Bowser_is_best))
